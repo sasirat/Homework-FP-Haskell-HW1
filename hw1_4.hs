5c6170537b42515a437d7ac83dc711c37ce6f632
ekawitjaidee <35341892+ekawitjaidee@users.noreply.github.com>
2020-01-24 11:37:28 +0700

    Update hw1_4.hs

--- hw1_4.hs
+++ hw1_4.hs
@@ -1,4 +1,8 @@
 transpose :: Eq a => [[a]] -> [[a]]
+transpose [[]] = []
+transpose (xs:[]) = []
+transpose ([]:xs) = []
+transpose (xs:[]:ys) = []
 transpose x 
             | x /= [] = (map head x ): transpose (map tail x) 
             | otherwise  = []
